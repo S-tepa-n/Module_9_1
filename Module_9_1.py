eb7680e81ed0fb204936427ac8b1bb57126fda4e
Некрасов Степан <118102761+S-tepa-n@users.noreply.github.com>
2024-11-27 01:58:20 +0300

    Module_9_1.py

--- Module_9_1.py
+++ Module_9_1.py
@@ -5,4 +5,5 @@ def apply_all_func(int_list, *functions):
     return reuslts
 
 
-print(apply_all_func([6, 20, 15, 9], len, sum, sorted, max, min))
+print(apply_all_func([6, 20, 15, 9], max, min))
+print(apply_all_func([6, 20, 15, 9], len, sum, sorted))
